f7ca25beba689f5c1f1c6f98b5d2a93290653abb
austin <austinlee199723@yahoo.com>
2021-09-07 15:51:34 -0700

    added api folder

--- commands.bat
+++ commands.bat
@@ -1,1 +1,1 @@
-cd client && npm run build && cd .. && git add . && git commit -m "edited vercel.json" && git push origin main
+cd client && npm run build && cd .. && git add . && git commit -m "added api folder" && git push origin main
