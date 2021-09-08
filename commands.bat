40267a44896c0165594f6a890ff15faabc6ae305
austin <austinlee199723@yahoo.com>
2021-09-07 22:44:16 -0700

    added headers

--- commands.bat
+++ commands.bat
@@ -1,1 +1,1 @@
- cd client && npm run build && cd ..&& git add . && git commit -m "added responsive css" && git push origin main
+ cd client && npm run build && cd ..&& git add . && git commit -m "added headers" && git push origin main
