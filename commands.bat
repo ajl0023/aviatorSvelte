7e00d2a1f8f031e17cfdafa61672c69a4983c978
austin <austinlee199723@yahoo.com>
2021-09-07 21:47:57 -0700

    added headers file

--- commands.bat
+++ commands.bat
@@ -1,1 +1,1 @@
- cd client && npm run build && cd ..&& git add . && git commit -m "few css error fixes 3" && git push origin main
+ cd client && npm run build && cd ..&& git add . && git commit -m "added headers file" && git push origin main
