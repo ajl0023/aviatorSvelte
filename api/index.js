d11d2a3c38b07bf8ac8df6dcb688e3e22d99498c
austin <austinlee199723@yahoo.com>
2021-09-07 19:11:21 -0700

    netlify config

--- api/index.js
+++ api/index.js
@@ -6,7 +6,9 @@ const serverless = require("serverless-http");
 // or with callback
 
 const router = express.Router();
-router.get("/", (req, res) => {
+
+router.get("/test", (req, res) => {
+  console.log("reeeeeeeeeeeeeee");
   res.writeHead(200, { "Content-Type": "text/html" });
   res.write("<h1>Hello from Express.js!</h1>");
   res.end();
@@ -14,8 +16,7 @@ router.get("/", (req, res) => {
 router.get("/another", (req, res) => res.json({ route: req.originalUrl }));
 router.post("/", (req, res) => res.json({ postBody: req.body }));
 
-app.use("/.netlify/functions/server", router); // path must route to lambda
-app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
+// app.use("/.netlify/functions/server", router); // path must route to lambda
 
 module.exports = app;
 module.exports.handler = serverless(app);
