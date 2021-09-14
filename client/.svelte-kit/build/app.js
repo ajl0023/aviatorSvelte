d09b36f5c9ce3ce2663f83b11cc5156df017f63d
ajl0023 <austinlee199723@yahoo.com>
2021-09-14 14:01:08 -0700

    fixing merged conflicts

--- client/.svelte-kit/build/app.js
+++ client/.svelte-kit/build/app.js
@@ -4,11 +4,7 @@ import { set_paths, assets } from './runtime/paths.js';
 import { set_prerendering } from './runtime/env.js';
 import * as user_hooks from "./hooks.js";
 
-<<<<<<< HEAD
 const template = ({ head, body }) => "<!DOCTYPE html>\n<html lang=\"en\">\n\t<head>\n\t\t<meta charset=\"utf-8\" />\n\t\t<link rel=\"icon\" href=\"/logo.inline.svg\" />\n\t\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />\n\t\t" + head + "\n\t</head>\n\t<body>\n\t\t<div id=\"svelte\">" + body + "</div>\n\t</body>\n</html>\n";
-=======
-const template = ({ head, body }) => "<!DOCTYPE html>\r\n<html lang=\"en\">\r\n\t<head>\r\n\t\t<meta charset=\"utf-8\" />\r\n\t\t<link rel=\"icon\" href=\"/logo.inline.svg\" />\r\n\t\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />\r\n\t\t" + head + "\r\n\t</head>\r\n\t<body>\r\n\t\t<div id=\"svelte\">" + body + "</div>\r\n\t</body>\r\n</html>\r\n";
->>>>>>> bf4bdecb92398b2e1375113f515ab94f4bdee812
 
 let options = null;
 
@@ -26,15 +22,9 @@ export function init(settings = default_settings) {
 		amp: false,
 		dev: false,
 		entry: {
-<<<<<<< HEAD
 			file: assets + "/_app/start-d099ea12.js",
 			css: [assets + "/_app/assets/start-61d1577b.css"],
 			js: [assets + "/_app/start-d099ea12.js",assets + "/_app/chunks/vendor-9b70e41c.js"]
-=======
-			file: assets + "/_app/start-4cc6516e.js",
-			css: [assets + "/_app/assets/start-61d1577b.css"],
-			js: [assets + "/_app/start-4cc6516e.js",assets + "/_app/chunks/vendor-9b70e41c.js"]
->>>>>>> bf4bdecb92398b2e1375113f515ab94f4bdee812
 		},
 		fetched: undefined,
 		floc: false,
@@ -66,11 +56,7 @@ const d = decodeURIComponent;
 const empty = () => ({});
 
 const manifest = {
-<<<<<<< HEAD
 	assets: [{"file":"_headers.txt","size":32,"type":"text/plain"},{"file":"logo.inline.svg","size":37724,"type":"image/svg+xml"}],
-=======
-	assets: [{"file":"logo.inline.svg","size":37735,"type":"image/svg+xml"},{"file":"_headers.txt","size":35,"type":"text/plain"}],
->>>>>>> bf4bdecb92398b2e1375113f515ab94f4bdee812
 	layout: ".svelte-kit/build/components/layout.svelte",
 	error: ".svelte-kit/build/components/error.svelte",
 	routes: [
@@ -94,17 +80,10 @@ const get_hooks = hooks => ({
 });
 
 const module_lookup = {
-<<<<<<< HEAD
 	".svelte-kit/build/components/layout.svelte": () => import("./components/layout.svelte"),".svelte-kit/build/components/error.svelte": () => import("./components/error.svelte"),"src/routes/index.svelte": () => import("../../src/routes/index.svelte")
 };
 
 const metadata_lookup = {".svelte-kit/build/components/layout.svelte":{"entry":"layout.svelte-c096017f.js","css":[],"js":["layout.svelte-c096017f.js","chunks/vendor-9b70e41c.js"],"styles":[]},".svelte-kit/build/components/error.svelte":{"entry":"error.svelte-527e4908.js","css":[],"js":["error.svelte-527e4908.js","chunks/vendor-9b70e41c.js"],"styles":[]},"src/routes/index.svelte":{"entry":"pages/index.svelte-57cfb59a.js","css":["assets/pages/index.svelte-4314b938.css"],"js":["pages/index.svelte-57cfb59a.js","chunks/vendor-9b70e41c.js"],"styles":[]}};
-=======
-	".svelte-kit/build/components/layout.svelte": () => import("./components\\layout.svelte"),".svelte-kit/build/components/error.svelte": () => import("./components\\error.svelte"),"src/routes/index.svelte": () => import("..\\..\\src\\routes\\index.svelte")
-};
-
-const metadata_lookup = {".svelte-kit/build/components/layout.svelte":{"entry":"layout.svelte-568fd069.js","css":[],"js":["layout.svelte-568fd069.js","chunks/vendor-9b70e41c.js"],"styles":[]},".svelte-kit/build/components/error.svelte":{"entry":"error.svelte-6bc0041d.js","css":[],"js":["error.svelte-6bc0041d.js","chunks/vendor-9b70e41c.js"],"styles":[]},"src/routes/index.svelte":{"entry":"pages/index.svelte-d3171d28.js","css":["assets/pages/index.svelte-e993cab7.css"],"js":["pages/index.svelte-d3171d28.js","chunks/vendor-9b70e41c.js"],"styles":[]}};
->>>>>>> bf4bdecb92398b2e1375113f515ab94f4bdee812
 
 async function load_component(file) {
 	const { entry, css, js, styles } = metadata_lookup[file];
--- commands.sh
+++ commands.sh
@@ -1,1 +1,1 @@
- cd client && npm run build && cd ..&& git add . && git commit -m "uploading temporary bg photo" && git push origin main
+ cd client && npm run build && cd ..&& git add . && git commit -m "fixing merged conflicts" && git push origin main
