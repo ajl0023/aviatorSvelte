c24a93c5c59e12ebb78324c1b0fbd2cfe447ca33
ajl0023 <austinlee199723@yahoo.com>
2021-09-23 18:33:41 -0700

    few indexing fixes and added contact us

---
+++ netlify/functions/submission-created.js
@@ -0,0 +1,44 @@
+var nodemailer = require("nodemailer");
+
+exports.handler = function (event, context, callback) {
+  const body = JSON.parse(event.body);
+  const payload = body.payload.human_fields;
+
+  const transporter = nodemailer.createTransport({
+    service: "yahoo",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+  var mailOptions = {
+    from: process.env.EMAIL,
+    to: payload.Email,
+    subject: "Maliview application: Thank you",
+    html: `<h1>Hi, ${payload.Name}</h1><p>Info about maliview</p>`,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      callback(null, { statusCode: 200 });
+    }
+  });
+  transporter.sendMail(
+    {
+      from: process.env.EMAIL,
+      to: process.env.EMAIL,
+      subject: "Maliview application",
+      html: /* HTML */ `<p>Name: ${payload.Name}</p>
+        <p>Email: ${payload.Email}</p>
+        <p>Phone: ${payload.Phone}</p>
+        <p>Message: ${payload.Message}</p>`,
+    },
+    function (error, info) {
+      if (error) {
+        console.log(error);
+      }
+    }
+  );
+};
